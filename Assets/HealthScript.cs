3ae0d86218b07bcfa3569cd0e1c3ce4ff086f9fb
DanielMai <danielmai122001@gmail.com>
2021-01-09 15:29:44 -0600

    update Health and UI in game

---
+++ Assets/HealthScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthScript : MonoBehaviour
+{
+   // public Player player;
+    public Text healthText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //healthText = "HEALTH: " + player.GetHealth().ToString();
+    }
+}
