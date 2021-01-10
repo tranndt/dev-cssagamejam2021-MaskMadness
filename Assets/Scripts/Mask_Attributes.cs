e298b4c7e66c094518643883ce94e8effd3a97be
Quoc Vi Nguyen <charlesivnguyen4@yahoo.com>
2021-01-10 10:28:22 -0600

    Fix mask and add halo effect

--- Assets/Scripts/Mask_Attributes.cs
+++ Assets/Scripts/Mask_Attributes.cs
@@ -9,6 +9,11 @@ public class Mask_Attributes : MonoBehaviour
     private System.Random rnd = new System.Random();
     [SerializeField] float protect_value;
 
+    //
+    float angle = 360.0f; // Degree per time unit
+    float time = 1.0f; // Time unit in sec
+    Vector3 axis = Vector3.up; // Rotation axis, here it the yaw axis
+
     public float Get_Value()
     {   
         return protect_value;
@@ -30,6 +35,6 @@ public class Mask_Attributes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.RotateAround(transform.position, Vector3.up, 40 * Time.deltaTime);
     }
 }
