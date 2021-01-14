040dacea70d7bbcf21f42c55695fe046e68106aa
Quoc Vi Nguyen <charlesivnguyen4@yahoo.com>
2021-01-14 16:31:48 -0600

    Successfully improve car movement along given path
    Also renew player and car prefabs

--- Assets/Scripts/Car_AI.cs
+++ Assets/Scripts/Car_AI.cs
@@ -10,16 +10,22 @@ public class Car_AI : MonoBehaviour
     private float distance;
     private CharacterController controller;
     [SerializeField] float carSpeed;
+    [SerializeField] float carCurvative;
 
     // Start is called before the first frame update
     void Start()
-    {
-        wayPoints = path.GetComponentsInChildren<Transform>();
+    {   
+        //Get the nodes on the path
+        wayPoints = path.GetComponent<Path>().Get_Nodes();
+        
+        //Get the character controller for movement
         controller = gameObject.GetComponent<CharacterController>();
 
+        //Set the default node index, but will be replace with the nearest one
         wayPointIndex = 0;
         float min =  Vector3.Distance(transform.position, wayPoints[0].position);
 
+        //Find the nearest node to head to
         for(int i = 0; i < wayPoints.Length; i++)
         {
             distance = Vector3.Distance(transform.position, wayPoints[i].position);
@@ -30,61 +36,49 @@ public class Car_AI : MonoBehaviour
             }
         }
 
+        //Make the car look at the nearest node
         transform.LookAt(wayPoints[wayPointIndex].position);
+
+        //Set car speed
         carSpeed = 8f;
+
+        //Set car curvative
+        carCurvative = 3.5f;
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         MoveToPoint();
     }
 
     private void IncreaseIndex()
     {
         wayPointIndex++;
+
         if (wayPointIndex >= wayPoints.Length)
         {
             wayPointIndex = 0;
         }
-        //transform.LookAt(wayPoints[wayPointIndex].position);
     }
 
     private void MoveToPoint()
     {
         distance = Vector3.Distance(transform.position, wayPoints[wayPointIndex].position);
 
-        if (distance < 1f)
+        Vector3 direction = wayPoints[wayPointIndex].position - transform.position;
+
+        if (distance < 2f)
         {
-            PerformTurn();
+
             IncreaseIndex();
-            return;
         }
 
-        Vector3 moveDiff = wayPoints[wayPointIndex].position - transform.position;
-        Vector3 moveDir = moveDiff.normalized * carSpeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, carCurvative * Time.deltaTime);
+
+        Vector3 moveDir = transform.forward * carSpeed * Time.deltaTime;
 
         controller.Move(moveDir);
-    }
-
-    private void PerformTurn()
-    {
-        Vector3 currEdge;
-        Vector3 nextEdge;
-        if (wayPoints.Length > 2)
-        {
-            currEdge = wayPoints[(wayPointIndex + 1) % wayPoints.Length].position - wayPoints[wayPointIndex % wayPoints.Length].position;
-            nextEdge = wayPoints[(wayPointIndex + 2) % wayPoints.Length].position - wayPoints[(wayPointIndex + 1) % wayPoints.Length].position;
-
-            float angle = Vector3.Angle(currEdge, nextEdge);
-            Debug.Log(angle);
-            transform.rotation = Quaternion.Euler(0f, -angle, 0f);
-
-        }
-        else
-        {
-            return;
-        }
-
     }
 }
--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -40,4 +40,28 @@ public class Path : MonoBehaviour
             
         }
     }
+
+    private void Awake()
+    {
+        Transform[] pathTransforms = GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+    }
+
+    public Transform[] Get_Nodes()
+    {
+        Transform[] wayPoints = new Transform[nodes.Count];
+
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            wayPoints[i] = nodes[i];
+        }
+        return wayPoints;
+    }
 }
